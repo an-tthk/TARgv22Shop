2f563d67191497a48280f2d6bc8f2782e028c70d
Opilane TTHK <opilane@tthk.ee>
2023-09-11 12:06:17 +0300

    Controller

--- TARgv22Shop/Controllers/SpaceshipsController.cs
+++ TARgv22Shop/Controllers/SpaceshipsController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using TARgv22Shop.Models;
+using TARgv22Shop.Models.Spaceship;
 
 namespace TARgv22Shop.Controllers
 {
     public class SpaceshipsController : Controller
     {
+        private readonly ShopContext _context;
+        
+        public SpaceshipsController(ShopContext context)
+        {
+            this._context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Type = x.Type,
+                    Passengers = x.Passengers,
+                    EnginePower = x.EnginePower,
+                });
+
+            return View(result);
         }
 
         [HttpGet]
