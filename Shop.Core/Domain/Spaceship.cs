48375c51ff89b385364cf44e4b15ca8773b34b7f
Opilane TTHK <opilane@tthk.ee>
2023-09-11 10:34:43 +0300

    Shop migration

--- Shop.Core/Domain/Spaceship.cs
+++ Shop.Core/Domain/Spaceship.cs
@@ -11,8 +11,8 @@ namespace Shop.Core.Domain
         public int Passengers { get; set; }
         public int EnginePower { get; set; }
         public int Crew { get; set; }
-        public string FuelType { get; set; }
-        public int FuelCapacity { get; set; }
+        //public string FuelType { get; set; }
+        //public int FuelCapacity { get; set; }
         public string Company { get; set; }
         public int CargoWeight { get; set; }
 
