be7ce78e04afc0b5f8a87dc0b11e2c3d76a46727
Nurkz <contact@nurkz.dev>
2024-01-18 10:37:08 +0200

    LoginAndRegistrationInProgress

---
+++ TARgv22Shop/Models/Accounts/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TARgv22Shop.Utilities;
+
+namespace TARgv22Shop.Models.Accounts
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [ValidEmailDomain(allowedDomain: "gmail.com", ErrorMessage = "Email domain must be gmail.com")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+
+        public string City { get; set; }
+    }
+}
